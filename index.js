a0fc135fb5484b16d5c01f972304eb34b3100f30
Wir3s <113479857+Wir3s@users.noreply.github.com>
2022-12-30 08:57:44 -0600

    preparing array of questions and write file function

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
 const Employee = require('./employee');
 
 console.log(Employee);
 
 
+// Array of questions for user input
+const questions = [
+    {
+      type: "input",
+      name: "manager",
+      message: "Who is the project manager?",
+    },
+]
+
+
+
+
+ inquirer.prompt(questions).then((data) => writeToFile(data));
+
+// Function to write README file
+// function writeToFile(data) {
+//   console.log(data);
+//   fs.writeFile("./utils/README.MD", generate(data), (err) =>
+//     err ? console.log(err) : console.log("Success!")
+//   );
+// }
