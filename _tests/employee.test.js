491042e69719bb21e459c84c69758323683b024d
Wir3s <aandrebess@gmail.com>
2022-12-30 14:24:39 -0600

    Added test

--- _tests/employee.test.js
+++ _tests/employee.test.js
@@ -1,17 +1,29 @@
 const Employee = require("../lib/employee");
 
 describe("Employee", () => {
-  it("should return the name of the employee", () => {
+  it("should return the name and email of the employee", () => {
     // Arrange
 
     const name = "Andre";
+    const email = "andre@andre.com";
 
     // Act
 
-    const obj = new Employee(name);
+    const obj = new Employee(name, email);
 
     // Assert
 
     expect(obj.name).toEqual(name);
+    expect(obj.email).toEqual(email);
+  });
+
+  // Exception test
+  it("should throw an error if email address does not contain '@'", () => {
+    // Arrange
+    const cb = () => new Employee("Andre", "andregmail.com");
+    const err = new Error("Not a valid email address");
+
+    // Assert
+    expect(cb).toThrowError(err);
   });
 });
