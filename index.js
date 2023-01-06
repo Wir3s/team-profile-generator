55780701ba190ac2520bba0c1bbc8b0afd54e635
Wir3s <113479857+Wir3s@users.noreply.github.com>
2023-01-06 05:07:18 -0600

    CHanges

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const Employee = require("./lib/employee");
 const Manager = require("./lib/manager");
-const generate = require("./src/sample")
+const generate = require("./src/sample");
 
 console.log(Employee);
 console.log(Manager);
+
+const team = [];
 
 // Array of questions for user input
 const managerQuestions = [
@@ -29,11 +31,12 @@ const managerQuestions = [
     name: "office",
     message: "What is their office number?",
   },
-  // {
-  //   type: "list",
-  //   name: 
-
-  // },
+  {
+    type: "list",
+    name: "menu",
+    choices: ["Engineer", "Intern", "Done"],
+    message: "Do you have another team memeber to add?",
+  },
 ];
 
 inquirer.prompt(managerQuestions).then((data) => createManager(data));
@@ -46,6 +49,8 @@ function createManager(data) {
   const teamManager = new Manager(data.name, data.email, data.id, data.office);
   console.log(teamManager);
   writeToFile(teamManager);
+ // team.push(teamManager);
+  console.log(team);
 }
 
 function writeToFile(data) {
@@ -54,5 +59,3 @@ function writeToFile(data) {
     err ? console.log(err) : console.log("Success!")
   );
 }
-// const Andre = new Manager("Andrew", "wires@gmail.com", 45, 7);
-// console.log(Andre);
